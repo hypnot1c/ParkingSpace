6591c68c982d925c3741ef4511eea7bea1d3e557
hypnot1c <ahmed_@mail.ru>
2021-09-12 23:29:25 +0300

    Parking place's tables

---
+++ src/DataBases/ParkingSpace/dbo/Tables/Parking_Group.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Parking_Group]
+(
+  [Id] INT NOT NULL IDENTITY,
+  [Name] NVARCHAR(200) NOT NULL,
+
+  CONSTRAINT [PK_Parking_Group_Id] PRIMARY KEY ([Id]),
+)
---
+++ src/DataBases/ParkingSpace/dbo/Tables/Parking_Place.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Parking_Place]
+(
+  [Id] INT NOT NULL IDENTITY,
+  [GroupId] INT NOT NULL,
+  [Number] NVARCHAR(100) NOT NULL,
+  [Comment] NVARCHAR(MAX) NULL,
+
+  CONSTRAINT [PK_Parking_Place_Id] PRIMARY KEY ([Id]),
+  CONSTRAINT [FK_Parking_Group_Id_Parking_Place_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [Parking_Group]([Id]),
+)
